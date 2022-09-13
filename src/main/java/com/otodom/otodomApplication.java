4334b4876d3e8df1beb8cce3cca1a99dbef4c71e
Szymon Kochanowski <szymon.kochanowski@fdmgroup.com>
2022-09-13 14:08:10 +0200

    Renamed application.

--- src/main/java/com/otodom/otodomApplication.java
+++ src/main/java/com/otodom/otodomApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThreadApplication {
+public class otodomApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ThreadApplication.class, args);
+		SpringApplication.run(otodomApplication.class, args);
 	}
 
 }
